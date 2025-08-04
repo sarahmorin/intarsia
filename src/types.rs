1bbe7b1f2b2561bf8ae557121bc951f616299e4f
sarahmorin <smorin8674@gmail.com>
2025-08-04 13:58:57 -0700

    basic e-graph implementation, minus extraction function
    unit tests passing
    
    added unit tests on egraph and fixed some bugs
    
    egraph updates to use OpOrVar pattern instead of nonsense I was doing before

--- src/types.rs
+++ src/types.rs
@@ -1,32 +1,137 @@
-use std::fmt::{Debug, Display};
+use indexmap::IndexMap;
 use lattices::Lattice;
-
+use std::{
+    fmt::{Debug, Display},
+    hash::Hash,
+};
 
 /// Type to represent a unique identifier for an entity.
 pub type Id = usize;
+pub type MulteId = (Id, Id);
+
+/// Type to represent a variable in the language
+pub type Var = String;
 
 /// Operator trait for expressions and terms.
 
-
 /// Alias for Expr/Term types used in the egraph.
+///
+/// Typically, an AST is an enum or a struct that represents the operations in the expression or term.
+/// For example, it could be an enum with variants for different operations like `Add`, `Sub`, etc.
+
+/// ```
+/// pub enum MyOp {
+///     Add,
+///     Sub,
+///     Mul,
+///     Div,
+///     // Terminal values can also be included
+///     Const(i32),
+///     Var(String),
+/// }
+/// ```
 // FIXME: Come up with a better name for this trait.
-pub trait AST: Clone + Debug + PartialEq + Eq {}
-impl<T> AST for T where T: Clone + Debug + PartialEq + Eq {}
+pub trait AST: Clone + Debug + PartialEq + Eq + Display + Hash {}
 
 /// Alias for generic analysis type.
 pub trait Analysis: Lattice + Clone + Debug + PartialEq + Eq {
     /// Creates a default instance of the analysis type.
     fn default() -> Self;
 }
-impl<T> Analysis for T where T: Lattice + Clone + Debug + PartialEq + Eq {
-    // FIXME: deal with this
-    fn default() -> Self {
-        // Default implementation can be provided here if needed
-        unimplemented!()
+
+/// Property trait for properties of operators and terms.
+pub trait Property: Clone + Debug + PartialEq + Eq + PartialOrd + Display + Hash {
+    /// Compares two properties for containment.
+    /// NOTE: This is basically >= but for now I like the separate name so we can avoid accidentally using a derived `PartialOrd` trait.
+    fn contains(&self, other: &Self) -> bool;
+
+    /// Returns the "no properties" bottom element of the property set
+    fn bottom() -> Self;
+
+    /// Returns a vector of `n` bottom elements of the property set.
+    fn n_bottoms(n: usize) -> Vec<Self>
+    where
+        Self: Sized,
+    {
+        vec![Self::bottom(); n]
     }
 }
 
-/// Generic Expression structure
+/// Type alias for a substitution map.
+/// Maps variables (of type T) to their corresponding ENode IDs (Id).
+/// This is used to represent substitutions in the context of pattern matching and rewriting.
+pub type Subst<T, I> = IndexMap<T, I>;
+
+/// Information about an operator in the language.
+/// Contains its arity, output properties, and input properties indexed by argument index
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct OpInfo<P>
+where
+    P: Property,
+{
+    /// The arity of the operator.
+    pub arity: usize,
+    /// Properties of operator output
+    pub output_props: P,
+    /// Properties of operator input indexed by argument index.
+    /// If an argument does not have a property requirement,
+    /// its value in the vec should satisfy `P.is_bot()`, i.e., the bottom element of the lattice.
+    pub input_props: Vec<P>,
+}
+
+impl<P> OpInfo<P>
+where
+    P: Property,
+{
+    /// Creates a new OpInfo instance with the given operator, arity, output properties, and input properties.
+    pub fn new(arity: usize, output_props: P, input_props: Vec<P>) -> Self {
+        Self {
+            arity,
+            output_props,
+            input_props,
+        }
+    }
+
+    /// Returns a default OpInfo with given arity and no properties
+    pub fn default(arity: usize) -> Self {
+        Self {
+            arity,
+            output_props: P::bottom(),
+            input_props: P::n_bottoms(arity),
+        }
+    }
+}
+
+impl<P> Display for OpInfo<P>
+where
+    P: Property,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "OpInfo(arity: {}, output_props: {}, input_props: {:?})",
+            self.arity, self.output_props, self.input_props
+        )
+    }
+}
+
+/// Trait for languages over operators and properties.
+/// The `PropLang` trait defines the interface for mapping from Expressions to operator information.
+///
+/// When defining a new language:
+///     - define a set of operators with the AST trait
+///     - define a set of properties with the Property trait (requires implementing PartialOrd and Display)
+///     - implement From<T> for P
+///     - implement From<Expr<T>> for OpInfo<P>
+pub trait PropLang<T, P>
+where
+    T: AST,
+    P: Property + From<T>,
+    OpInfo<P>: From<Expr<T>>,
+{}
+
+/// Generic Recursive Expression structure
+/// Typically we use the expression to represent the input expression to the system
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Expr<T>
 where
@@ -54,20 +159,76 @@ where
     pub fn args(&self) -> &Vec<Expr<T>> {
         &self.args
     }
+
+    /// Returns true if expr is terminal, i.e., has no arguments.
+    pub fn is_terminal(&self) -> bool {
+        self.args.is_empty()
+    }
 }
 
 impl<T> Display for Expr<T>
 where
-    T: AST + Display,
+    T: AST,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "Expr(op: {}, args: {:?})", self.op, self.args)
     }
 }
 
+/// Enum to represent either an expression or a variable.
+/// This is useful for representing rewrite rules
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum OpOrVar<T>
+where
+    T: AST,
+{
+    Op(T),
+    Var(Var),
+}
+
+impl<T> Display for OpOrVar<T>
+where
+    T: AST,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            OpOrVar::Op(op) => write!(f, "OpOrVar::Op({})", op),
+            OpOrVar::Var(var) => write!(f, "OpOrVar::Var({})", var),
+        }
+    }
+}
+
+impl<T> OpOrVar<T>
+where
+    T: AST,
+{
+    pub fn is_expr(&self) -> bool {
+        matches!(self, OpOrVar::Op(_))
+    }
+
+    pub fn is_var(&self) -> bool {
+        matches!(self, OpOrVar::Var(_))
+    }
+}
+
+impl<T> AST for OpOrVar<T> where
+    T: AST,
+{}
+
+impl<T> From<Var> for OpOrVar<T>
+where
+    T: AST,
+{
+    fn from(var: Var) -> Self {
+        OpOrVar::Var(var)
+    }
+}
+
+pub type Pattern<T> = Expr<OpOrVar<T>>;
+
 /// Generic Term structure
 /// Represents a term in a hashconsed structure.
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Term<T>
 where
     T: AST,
@@ -93,5 +254,94 @@ where
     /// Returns the arguments of the term.
     pub fn args(&self) -> &Vec<Id> {
         &self.args
+    }
+
+    /// Returns true if Expr matches the term in operator and arity.
+    pub fn matches_expr(&self, expr: &Expr<T>) -> bool {
+        self.op() == expr.op() && self.args().len() == expr.args().len()
+    }
+
+    /// Returns true if term matches pattern in operator and arity
+    /// Note: This does not check properties or arguments.
+    pub fn matches_pattern(&self, pattern: &Pattern<T>) -> bool {
+        match pattern.op() {
+            OpOrVar::Op(t) => self.op() == t && self.args().len() == pattern.args().len(),
+            OpOrVar::Var(_) => true, // If the pattern is a variable, it matches any term
+        }
+    }
+}
+
+/// Generic MulteTerm structure
+/// Represents a term in a hashconsed structure.
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct MulteTerm<T, P>
+where
+    T: AST,
+    P: Property,
+{
+    op: T,
+    args: Vec<MulteId>,
+    props: P,
+}
+
+impl<T, P> MulteTerm<T, P>
+where
+    T: AST + Into<P>,
+    P: Property + From<T>,
+    OpInfo<P>: From<Expr<T>>,
+    Expr<T>: PropLang<T, P>,
+{
+    /// Creates a new term with the given operator and arguments.
+    pub fn new(op: T, args: Vec<MulteId>, props: P) -> Self {
+        Self { op, args, props }
+    }
+
+    /// Returns the operator of the term.
+    pub fn op(&self) -> &T {
+        &self.op
+    }
+
+    /// Returns the properties of the term.
+    pub fn props(&self) -> &P {
+        &self.props
+    }
+
+    /// Returns the arguments of the term.
+    pub fn args(&self) -> &Vec<MulteId> {
+        &self.args
+    }
+
+    /// Returns true if Expr matches the term in operator and arity.
+    /// Note: This does not check properties or arguments.
+    pub fn matches_expr(&self, expr: &Expr<T>) -> bool {
+        self.op() == expr.op() && self.args().len() == expr.args().len()
+    }
+
+    /// Returns true if term matches pattern in operator and arity
+    /// Note: This does not check properties or arguments.
+    pub fn matches_pattern(&self, pattern: &Pattern<T>) -> bool {
+        match pattern.op() {
+            OpOrVar::Op(t) => self.op() == t && self.args().len() == pattern.args().len(),
+            OpOrVar::Var(_) => true, // If the pattern is a variable, it matches any term
+        }
+    }
+
+    /// Returns true if the term satisfies a given set of properties
+    /// by checking if the term's properties contain the given property.
+    pub fn satisfies_property(&self, prop: &P) -> bool {
+        self.props.contains(prop)
+    }
+}
+
+impl<T, P> Into<Term<T>> for MulteTerm<T, P>
+where
+    T: AST,
+    P: Property,
+{
+    fn into(self) -> Term<T> {
+        Term::new(
+            self.op,
+            self.args.into_iter().map(|(id1, _id2)| id1).collect(),
+        )
     }
 }
--- src/unionfind.rs
+++ src/unionfind.rs
@@ -52,10 +52,13 @@ impl UnionFind {
 
         if root1 < root2 {
             self.parent[root2] = root1;
+            root1
         } else if root2 < root1 {
             self.parent[root1] = root2;
+            root2
+        } else {
+            root1
         }
-        root1
     }
 
     /// Get list of all root elements.
@@ -67,5 +70,251 @@ impl UnionFind {
             }
         }
         roots
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let uf = UnionFind::new();
+        assert_eq!(uf.parent.len(), 0);
+        assert_eq!(uf.roots(), vec![]);
+    }
+
+    #[test]
+    fn test_add_set() {
+        let mut uf = UnionFind::new();
+
+        // Adding first set should return ID 0
+        let id0 = uf.add_set();
+        assert_eq!(id0, 0);
+        assert_eq!(uf.parent.len(), 1);
+        assert_eq!(uf.parent[0], 0); // Should be its own parent
+
+        // Adding second set should return ID 1
+        let id1 = uf.add_set();
+        assert_eq!(id1, 1);
+        assert_eq!(uf.parent.len(), 2);
+        assert_eq!(uf.parent[1], 1); // Should be its own parent
+
+        // Adding third set should return ID 2
+        let id2 = uf.add_set();
+        assert_eq!(id2, 2);
+        assert_eq!(uf.parent.len(), 3);
+        assert_eq!(uf.parent[2], 2); // Should be its own parent
+    }
+
+    #[test]
+    fn test_find_single_elements() {
+        let mut uf = UnionFind::new();
+        let id0 = uf.add_set();
+        let id1 = uf.add_set();
+        let id2 = uf.add_set();
+
+        // Each element should be its own root initially
+        assert_eq!(uf.find(id0), 0);
+        assert_eq!(uf.find(id1), 1);
+        assert_eq!(uf.find(id2), 2);
+    }
+
+    #[test]
+    fn test_find_after_union() {
+        let mut uf = UnionFind::new();
+        let id0 = uf.add_set();
+        let id1 = uf.add_set();
+        let id2 = uf.add_set();
+
+        // Union 1 and 2 - smaller root (1) should become parent
+        let root = uf.union(id1, id2);
+        assert_eq!(root, 1); // Should return the smaller root
+
+        // Both should now have the same root
+        assert_eq!(uf.find(id1), 1);
+        assert_eq!(uf.find(id2), 1);
+
+        // id0 should still be separate
+        assert_eq!(uf.find(id0), 0);
+    }
+
+    #[test]
+    fn test_union_behavior() {
+        let mut uf = UnionFind::new();
+        let id0 = uf.add_set();
+        let id1 = uf.add_set();
+        let id2 = uf.add_set();
+        let id3 = uf.add_set();
+
+        // Test that smaller root becomes parent
+        uf.union(id2, id0); // Union 2 and 0, smaller root (0) should become parent
+        assert_eq!(uf.find(id2), 0); // 2 should now have root 0
+        assert_eq!(uf.find(id0), 0); // 0 should still have root 0
+
+        uf.union(id3, id1); // Union 3 and 1, smaller root (1) should become parent
+        assert_eq!(uf.find(id3), 1); // 3 should now have root 1
+        assert_eq!(uf.find(id1), 1); // 1 should still have root 1
+
+        // Union the two groups: roots 0 and 1, so 0 should become the parent
+        uf.union(id0, id1); // Union groups with roots 0 and 1
+
+        // All elements should now have root 0 (the smaller root)
+        assert_eq!(uf.find(id0), 0);
+        assert_eq!(uf.find(id1), 0);
+        assert_eq!(uf.find(id2), 0);
+        assert_eq!(uf.find(id3), 0);
+    }
+
+    #[test]
+    fn test_union_same_set() {
+        let mut uf = UnionFind::new();
+        let id0 = uf.add_set();
+        let id1 = uf.add_set();
+
+        // Union them first
+        uf.union(id0, id1);
+
+        // Union again - should not change anything
+        let root = uf.union(id0, id1);
+        assert_eq!(root, 0); // Should still return the root
+        assert_eq!(uf.find(id0), 0);
+        assert_eq!(uf.find(id1), 0);
+    }
+
+    #[test]
+    fn test_find_compress() {
+        let mut uf = UnionFind::new();
+        let id0 = uf.add_set();
+        let id1 = uf.add_set();
+        let id2 = uf.add_set();
+        let id3 = uf.add_set();
+
+        // Create a chain: 3 -> 2 -> 1 -> 0
+        uf.union(id0, id1); // 0 becomes root
+        uf.union(id0, id2); // 0 remains root
+        uf.union(id0, id3); // 0 remains root
+
+        // Before compression, let's verify the structure
+        assert_eq!(uf.find(id3), 0);
+
+        // Use find_compress on id3
+        let root = uf.find_compress(id3);
+        assert_eq!(root, 0);
+
+        // After compression, id3 should point directly to root
+        // This tests that path compression actually occurred
+        assert_eq!(uf.find_compress(id3), 0);
+    }
+
+    #[test]
+    fn test_roots() {
+        let mut uf = UnionFind::new();
+
+        // Empty case
+        assert_eq!(uf.roots(), vec![]);
+
+        // Single element
+        let id0 = uf.add_set();
+        assert_eq!(uf.roots(), vec![0]);
+
+        // Two separate elements
+        let id1 = uf.add_set();
+        let mut roots = uf.roots();
+        roots.sort(); // Sort for consistent comparison
+        assert_eq!(roots, vec![0, 1]);
+
+        // After union, should have only one root
+        uf.union(id0, id1);
+        assert_eq!(uf.roots(), vec![0]);
+
+        // Add more elements
+        let id2 = uf.add_set();
+        let id3 = uf.add_set();
+        let mut roots = uf.roots();
+        roots.sort();
+        assert_eq!(roots, vec![0, 2, 3]);
+
+        // Union two more
+        uf.union(id2, id3);
+        let mut roots = uf.roots();
+        roots.sort();
+        assert_eq!(roots, vec![0, 2]);
+    }
+
+    #[test]
+    fn test_complex_operations() {
+        let mut uf = UnionFind::new();
+
+        // Create 6 sets
+        let ids: Vec<Id> = (0..6).map(|_| uf.add_set()).collect();
+
+        // Initially, all should be separate roots
+        assert_eq!(uf.roots().len(), 6);
+
+        // Create some unions: (0,1), (2,3), (4,5)
+        uf.union(ids[0], ids[1]);
+        uf.union(ids[2], ids[3]);
+        uf.union(ids[4], ids[5]);
+
+        // Should have 3 roots now
+        assert_eq!(uf.roots().len(), 3);
+
+        // Union two groups: (0,1) with (2,3)
+        uf.union(ids[0], ids[2]);
+
+        // Should have 2 roots now
+        assert_eq!(uf.roots().len(), 2);
+
+        // Check that elements are in correct groups
+        let root_01 = uf.find(ids[0]);
+        assert_eq!(uf.find(ids[1]), root_01);
+        assert_eq!(uf.find(ids[2]), root_01);
+        assert_eq!(uf.find(ids[3]), root_01);
+
+        let root_45 = uf.find(ids[4]);
+        assert_eq!(uf.find(ids[5]), root_45);
+
+        // Final union
+        uf.union(ids[0], ids[4]);
+
+        // All should have same root now
+        let final_root = uf.find(ids[0]);
+        for id in ids {
+            assert_eq!(uf.find(id), final_root);
+        }
+
+        // Should have exactly 1 root
+        assert_eq!(uf.roots().len(), 1);
+    }
+
+    #[test]
+    fn test_path_compression_effectiveness() {
+        let mut uf = UnionFind::new();
+
+        // Create a long chain by careful unions
+        let ids: Vec<Id> = (0..5).map(|_| uf.add_set()).collect();
+
+        // Manually create a chain by setting parents directly
+        // This simulates what could happen with multiple unions
+        uf.union(ids[0], ids[1]);
+        uf.union(ids[1], ids[2]);
+        uf.union(ids[2], ids[3]);
+        uf.union(ids[3], ids[4]);
+
+        // All should have the same root (0, since it's smallest)
+        let root = uf.find(ids[4]);
+        assert_eq!(root, 0);
+
+        // Now use find_compress on the deepest element
+        let compressed_root = uf.find_compress(ids[4]);
+        assert_eq!(compressed_root, 0);
+
+        // All subsequent finds should be fast
+        assert_eq!(uf.find(ids[4]), 0);
+        assert_eq!(uf.find(ids[3]), 0);
+        assert_eq!(uf.find(ids[2]), 0);
+        assert_eq!(uf.find(ids[1]), 0);
+        assert_eq!(uf.find(ids[0]), 0);
     }
 }
