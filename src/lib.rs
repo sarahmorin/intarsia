12011c52178b6443bfa1898addb1695be5f17e87
Sarah Morin <sarahmorin@berkeley.edu>
2026-03-10 13:25:39 -0700

    bugfix: no more build module, separate crate

--- src/lib.rs
+++ src/lib.rs
@@ -154,10 +154,6 @@
 // Generic optimizer framework
 pub mod framework;
 
-// Build-time helpers (only available with "build-helpers" feature)
-#[cfg(feature = "build-helpers")]
-pub mod build;
-
 // Re-export commonly used items from framework
 pub use framework::{
     CostDomain, CostFunction, ExplorerHooks, OptimizerFramework, Property, PropertyAwareLanguage,
